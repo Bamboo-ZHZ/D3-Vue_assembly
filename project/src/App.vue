4cb928bc502598fe74a70f83b010b587adb51bd0
Bamboo-ZHZ <82492066+Bamboo-ZHZ@users.noreply.github.com>
2022-03-05 14:58:34 +0800

    init
    init

---
+++ project/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  
+  <!-- <graphup :data_time = data_up :data_dif = data_down> </graphup> -->
+  <RunPosition></RunPosition>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import graphup from './components/graphup.vue'
+import RunPosition from './components/RunPosition.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    graphup,
+    RunPosition
+  },
+  data(){
+    return{
+      data_up:[0.1,0.1,0.05,0.1,0.1,0.2,0.05,0.1,0.1,0.1],
+      data_down:[0.1,0.1,0.05,0.1,0.1,0.2,0.05,0.1,0.1,0.1]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ project/src/components/RunPosition.vue
@@ -0,0 +1,25 @@
+<template>
+    <p>111</p>
+    <div id="my_dataviz"></div>
+</template>
+
+<script>
+import * as d3 from 'd3'
+export default({
+    name: 'RunPosition',
+    data(){
+        return{
+
+        }
+    },
+    mounted(){
+        
+    }
+})
+
+
+</script>
+
+<style scoped>
+
+</style>
